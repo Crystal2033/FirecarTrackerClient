e7634a189b3c4a36f242272f7b4715e2fee10127
yungtimofey <super.tim-stepanov@yandex.ru>
2022-07-07 00:47:32 +0300

    FR_7 made GpsInfoFragment

---
+++ app/src/main/java/com/aqulasoft/fireman/mobile/ui/postlogin/fragments/GpsInfoView.java
@@ -0,0 +1,23 @@
+/*
+ *  Owlsight MainView
+ *  Created by frostik0409@gmail.com
+ *  Kirill Stulnikov (Woipot)
+ *  on 29.09.20 17:14
+ *
+ *  Copyright © 2019 Petr Baev. All rights reserved.
+ *  Last modified 29.09.20 16:47
+ */
+
+package com.aqulasoft.fireman.mobile.ui.postlogin.fragments;
+
+import android.net.Uri;
+
+import com.aqulasoft.fireman.mobile.ui.base.ActivityBaseView;
+import com.aqulasoft.fireman.mobile.ui.base.BaseView;
+
+import moxy.viewstate.strategy.SkipStrategy;
+import moxy.viewstate.strategy.StateStrategyType;
+
+public interface GpsInfoView extends BaseView {
+
+}
